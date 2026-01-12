5ba7f07a2bb21e62aac18e5f84c4e399f1c332b0
Iglcyril <iglcyril@hotmail.fr>
2026-01-12 13:31:53 +0000

    fix: add comments to explain what is the correction

--- debugging/print_arguments.py
+++ debugging/print_arguments.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 import sys
-
+# we work with an array of string, in ther first case of this array there are the name of the script
+# so we start from index 1 to print only the arguments passed to the script
 for i in range(1, len(sys.argv)):
     print(sys.argv[i])
